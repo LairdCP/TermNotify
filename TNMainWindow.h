4f28ce2b250503e72f92550a9fa6b41d52249f1e
thedjnK <spam@helper3000.net>
2015-07-07 08:44:12 +0100

    v0.65b, new version number system

--- TNMainWindow.h
+++ TNMainWindow.h
@@ -47,7 +47,7 @@
 /******************************************************************************/
 #define DefaultDisplayTime 5000 //Time in mS to display a balloon message for (OS's can override this value)
 #define DefaultScanTime 800 //Time in mS to scan for new devices
-#define TermNotifyVer "Version 0.65" //Version string
+#define TermNotifyVer "v0.65b" //Version string
 
 /******************************************************************************/
 // Local Functions or Private Members
